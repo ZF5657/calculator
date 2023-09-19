fcc22768b3d31ac4f40ddf2f1ce5cf1bded8c848
ZF5657 <zane56fields57@gmail.com>
2023-09-19 17:32:18 -0400

    Add button, input, and display funtions
    Num btns display in display div / inputBox, inputBox filters out letters

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-const inputBox = document.querySelector(".input");
+const display = document.querySelector(".showNum");
 
-const clearBtn = document.querySelector(".clear");
+const inputBox = document.querySelector(".input");
 
 const clearAllBtn = document.querySelector(".clearAll");
 
@@ -14,27 +14,103 @@ const multiplyBtn = document.querySelector(".multiply");
 
 const divideBtn = document.querySelector(".divide");
 
-const oneBtn = document.querySelector("#one");
+const operators = document.querySelectorAll('.op');
 
-const twoBtn = document.querySelector("#two");
-
-const threeBtn = document.querySelector("#three");
-
-const fourBtn = document.querySelector("#four");
-
-const fiveBtn = document.querySelector("#five");
-
-const sixBtn = document.querySelector("#six");
-
-const sevenBtn = document.querySelector("#seven");
-
-const eightBtn = document.querySelector("#eight");
-
-const nineBtn = document.querySelector("#nine");
-
-const zeroBtn = document.querySelector("#zero");
+const numbers = document.querySelectorAll('.number');
 
 
+let num1 = '';
+let num2 = '';
+let operate = '';
+
+//function to restrict the input for the textbox (inputBox)
+function setInputFilter(textbox, inputFilter, errMsg) {
+    [ "input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop", "focusout" ].forEach(function(event) {
+      textbox.addEventListener(event, function(e) {
+        if (inputFilter(this.value)) {
+          // Accepted value.
+          if ([ "keydown", "mousedown", "focusout" ].indexOf(e.type) >= 0){
+            this.classList.remove("input-error");
+            this.setCustomValidity("");
+          }
+  
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        }
+        else if (this.hasOwnProperty("oldValue")) {
+          // Rejected value: restore the previous one.
+          this.classList.add("input-error");
+          this.setCustomValidity(errMsg);
+          this.reportValidity();
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        }
+        else {
+          // Rejected value: nothing to restore.
+          this.value = "";
+        }
+      });
+    });
+  };
+
+  setInputFilter(document.getElementById("inputBox"), function(value) {
+    return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp.
+  }, "Only digits and '.' are allowed");
+
+// Allows the calculator numbers to add their designated value into the input box
 const numInput = (val) => {
-    inputBox.value += val;
-}
+    num1 += val;
+};
+
+numbers.forEach((number) => number.addEventListener('click', function(e){
+    numInput(e.target.textContent);
+    inputBox.value = num1;
+}));
+
+
+// Clears the entries in the input box
+const clearInput = () => {
+    inputBox.value = '';
+    display.textContent = '';
+    num1 = '';
+    num2 = '';
+    operator = '';
+};
+
+
+const add = (num1, num2) => {
+    let solution = num1 + num2;
+    return solution;
+};
+
+const subtract = (num1, num2) => {
+    let solution = num1 - num2;
+    return solution
+};
+
+const multiply = (num1, num2) => {
+    let solution = num1 * num2;
+    return solution;
+};
+
+const divide = (num1, num2) => {
+    let solution = num1 / num2;
+    return solution;
+};
+
+
+const opProcessor = (op) => {
+    operator = op;
+    num2 = num1;
+    num1 = '';
+};
+
+
+operators.forEach((op) => {
+    op.addEventListener('click', function(e){
+        opProcessor(e.target.textContent)
+        display.textContent = num2 + " " + operator;
+        inputBox.value = '';
+    })
+});
