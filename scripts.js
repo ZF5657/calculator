53ee0fd607f4c9b7933bd2eb527af79738ed8fc0
ZF5657 <zane56fields57@gmail.com>
2023-09-26 11:24:12 -0400

    Fix equalBtn
    Isolated equalBtn to clear out inputBox

--- scripts.js
+++ scripts.js
@@ -1,24 +1,15 @@
 const display = document.querySelector(".showNum");
-
 const inputBox = document.querySelector(".input");
-
 const clearAllBtn = document.querySelector(".clearAll");
-
 const backspace = document.querySelector(".backspace");
-
 const operators = document.querySelectorAll('.op');
-
 const numbers = document.querySelectorAll('.number');
-
 const equalBtn = document.querySelector('.equals');
-
 
 display.style = ('font-size: medium')
 
-
 let num1 = '';
 let num2 = '';
-
 
 //function to restrict the input for the textbox (inputBox)
 function setInputFilter(textbox, inputFilter, errMsg) {
@@ -57,14 +48,14 @@ function setInputFilter(textbox, inputFilter, errMsg) {
 
 // Allows the calculator numbers to add their designated value into the input box
 const numInput = (val) => {
-    num1 += val;
+  num1 += val;
 };
 
 numbers.forEach((number) => number.addEventListener('click', function(e){
     numInput(e.target.textContent);
     inputBox.value = num1;
+    
 }));
-
 
 // Clears the entries in the input box
 const clearInput = () => {
@@ -74,7 +65,6 @@ const clearInput = () => {
     num2 = '';
     operator = '';
 };
-
 
 const add = (a, operator, b) => {
   if(operator === '+'){
@@ -109,6 +99,7 @@ const multiply = (a, operator, b) => {
 const divide = (a, operator, b) => {
   if(operator === '/'){
     if(a === 0 || b === 0) {
+      display.textContent = 'You cannot divide by zero!'
       return null;
     }
     else {
@@ -121,48 +112,48 @@ const divide = (a, operator, b) => {
   }
 };
 
-
 const opProcessor = (op) => {
     operator = op;
     num2 = num1;
     num1 = '';
 };
 
-
 operators.forEach((op) => {
     op.addEventListener('click', function(e){
-        opProcessor(e.target.textContent)
-        display.textContent = num2 + " " + operator;
-        inputBox.value = '';
+      opProcessor(e.target.textContent)
+      display.textContent = num2 + " " + operator;
+      inputBox.value = '';
+        
     })
-    equalBtn.addEventListener('click', function() {
-      operate()
-      inputBox.value = '';
-      if(operator === '/' && num1 === 0 || num2 === 0) {
-        display.textContent = 'You cannot divide by zero!'
-      }
-      else {
-        display.textContent = num2 + ` ` + operator + ` ` + num1 + ` ` + `= ` + solution;
-        display.style = ('font-weight: bold');
-      }
-      inputBox.value = '';
-    });
+    
+});
+
+equalBtn.addEventListener('click', function() {
+  operate()
+  inputBox.value = '';
+  if (operator === '/'){
+  }
+  else {
+    display.textContent = num2 + ` ` + operator + ` ` + num1 + ` ` + `= ` + solution;
+    display.style = ('font-weight: bold');
+  }
+  console.log(num1)
+  num1='';
 });
 
 const operate = () => {
   num1 = Number(num1);
   num2 = Number(num2);
-
   if(operator === '+') {
-    return add(num1, '+', num2)
+    return add(num2, '+', num1)
   }
   else if(operator === '-') {
-    return subtract(num1, '-', num2)
+    return subtract(num2, '-', num1)
   }
   else if(operator === '*') {
-    return multiply(num1, '*', num2)
+    return multiply(num2, '*', num1)
   }
   else if(operator === '/') {
-    return divide(num1, '/', num2)
+    return divide(num2, '/', num1)
   }
 };
