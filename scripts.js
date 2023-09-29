c226a41cfb132c903904e5db11539db19c713bf2
ZF5657 <zane56fields57@gmail.com>
2023-09-29 16:11:14 -0400

    Change display and add features
    Changes: removed text input box for div; Added: Dark/Light mode

--- scripts.js
+++ scripts.js
@@ -1,73 +1,31 @@
 const display = document.querySelector(".showNum");
-const inputBox = document.querySelector(".input");
+const inputBox = document.querySelector(".inputBox");
+const clearInput = document.querySelector(".clearInput");
 const clearAllBtn = document.querySelector(".clearAll");
-const backspace = document.querySelector(".backspace");
+const backBtn = document.querySelector(".backspace");
 const operators = document.querySelectorAll('.op');
 const numbers = document.querySelectorAll('.number');
 const equalBtn = document.querySelector('.equals');
+const darkBtn = document.querySelector('.dark');
 
-display.style = ('font-size: medium')
+const darkMode = () => {
+  document.body.classList.toggle('dark-theme');
+  if(darkBtn.textContent = 'Dark Mode') {
+    darkBtn.textContent = 'Light Mode';
+  }
+  else {
+    darkBtn.textContent = 'Dark Mode';
+  }
+}
 
-let num1 = '';
-let num2 = '';
+darkBtn.addEventListener('click', () => {
+  darkMode();
 
-//function to restrict the input for the textbox (inputBox)
-function setInputFilter(textbox, inputFilter, errMsg) {
-    [ "input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop", "focusout" ].forEach(function(event) {
-      textbox.addEventListener(event, function(e) {
-        if (inputFilter(this.value)) {
-          // Accepted value.
-          if ([ "keydown", "mousedown", "focusout" ].indexOf(e.type) >= 0){
-            this.classList.remove("input-error");
-            this.setCustomValidity("");
-          }
-  
-          this.oldValue = this.value;
-          this.oldSelectionStart = this.selectionStart;
-          this.oldSelectionEnd = this.selectionEnd;
-        }
-        else if (this.hasOwnProperty("oldValue")) {
-          // Rejected value: restore the previous one.
-          this.classList.add("input-error");
-          this.setCustomValidity(errMsg);
-          this.reportValidity();
-          this.value = this.oldValue;
-          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-        }
-        else {
-          // Rejected value: nothing to restore.
-          this.value = "";
-        }
-      });
-    });
-  };
+});
 
-  setInputFilter(document.getElementById("inputBox"), function(value) {
-    return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp.
-  }, "Only digits and '.' are allowed");
-
-// Allows the calculator numbers to add their designated value into the input box
-const numInput = (val) => {
-  num1 += val;
-};
-
-numbers.forEach((number) => number.addEventListener('click', function(e){
-    numInput(e.target.textContent);
-    inputBox.value = num1;
-    
-}));
-
-// Clears the entries in the input box
-const clearInput = () => {
-    inputBox.value = '';
-    display.textContent = '';
-    num1 = '';
-    num2 = '';
-    operator = '';
-};
-
+// adds functions to the operators
 const add = (a, operator, b) => {
-  if(operator === '+'){
+  if(operator === '+') {
     solution = a + b;
     return solution
   }
@@ -77,7 +35,7 @@ const add = (a, operator, b) => {
 };
 
 const subtract = (a, operator, b) => {
-  if(operator === '-'){
+  if(operator === '-') {
     solution = a - b;
     return solution
   }
@@ -87,17 +45,17 @@ const subtract = (a, operator, b) => {
 };
 
 const multiply = (a, operator, b) => {
-  if(operator === '*'){
+  if(operator === '*') {
     solution = a * b;
     return solution
   }
   else {
     return null;
   }
-};
+}
 
 const divide = (a, operator, b) => {
-  if(operator === '/'){
+  if(operator === '/') {
     if(a === 0 || b === 0) {
       display.textContent = 'You cannot divide by zero!'
       return null;
@@ -112,6 +70,46 @@ const divide = (a, operator, b) => {
   }
 };
 
+// const sqrRoot = (a, operator) => {
+//   if(operator === '√x') {
+//  
+// ***** NEEDS FUNCTION *****  
+//
+//     return solution;
+//   }
+//   else {
+//     return null
+//   }
+// }
+
+let num1 = '';
+let num2 = '';
+
+const numInput = (val) => {
+  num1 += val;
+};
+
+numbers.forEach((number) => number.addEventListener('click', (e) => {
+    numInput(e.target.textContent);
+    inputBox.textContent = num1;
+}));
+
+//adds a clear all function to the "C" button
+clearAllBtn.addEventListener('click', () => {
+  inputBox.textContent = '';
+  display.textContent = '';
+  num1 = '';
+  num2 = '';
+  operator = '';
+});
+
+//adds function to the clearInput button by clearing out the text content and num1
+//num1 is also clears so that it is not saved when resubmitting another number after clicking "CE"
+clearInput.addEventListener('click', () => {
+  inputBox.textContent = '';
+  num1 = '';
+});
+
 const opProcessor = (op) => {
     operator = op;
     num2 = num1;
@@ -119,25 +117,24 @@ const opProcessor = (op) => {
 };
 
 operators.forEach((op) => {
-    op.addEventListener('click', function(e){
+    op.addEventListener('click', (e) => {
       opProcessor(e.target.textContent)
       display.textContent = num2 + " " + operator;
-      inputBox.value = '';
+      inputBox.textContent = '';
         
     })
     
 });
 
-equalBtn.addEventListener('click', function() {
+equalBtn.addEventListener('click', () => {
   operate()
-  inputBox.value = '';
+  inputBox.textContent = '';
   if (operator === '/'){
   }
   else {
     display.textContent = num2 + ` ` + operator + ` ` + num1 + ` ` + `= ` + solution;
     display.style = ('font-weight: bold');
   }
-  console.log(num1)
   num1='';
 });
 
